a38bd4da00ef8a2ce0c6e057f9b99feaa80fe92f
Cesar Guzman <47696662+CesarGuzmanLopez@users.noreply.github.com>
2021-08-07 23:30:57 -0500

    ads

--- resources/views/mailrecor.blade.php
+++ resources/views/mailrecor.blade.php
@@ -1,7 +1,8 @@
-<p><b>Descripcion</b>{{ $Aviso->Descripcion }}</p>
-
-<p><b>Serie</b>{{ $Aviso->item->Serie }}</p>
-<p><b>Motor</b>{{ $Aviso->item->Motor }}</p>
-<p><b>Placas</b>{{ $Aviso->item->placas }}</p>
-<p><b>Poliza</b>{{ $Aviso->item->poliza  }}</p>
+<p><b>Descripcion:</b></p>
+<p>{{ $Aviso->Descripcion }}</p>
+<br>
+<p><b>Serie: </b>{{ $Aviso->item->Serie }}</p>
+<p><b>Motor: </b>{{ $Aviso->item->Motor }}</p>
+<p><b>Placas: </b>{{ $Aviso->item->placas }}</p>
+<p><b>Poliza: </b>{{ $Aviso->item->poliza  }}</p>
 <div style="margin-left: 15px;"><img src="http://recordatorios.gabrielsan.com/logo.gif" alt="" ></div>
